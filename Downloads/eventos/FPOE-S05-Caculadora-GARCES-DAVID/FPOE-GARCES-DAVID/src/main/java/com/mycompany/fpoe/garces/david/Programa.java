9645affa7ac7889c00331083ee0e216edc18df8b
DAVID ALEJANDEO GARCES <davida.lejandro.garces@correounivalle.edu.co>
2026-03-15 14:23:14 -0500

    dededdedded

---
+++ Downloads/eventos/FPOE-S05-Caculadora-GARCES-DAVID/FPOE-GARCES-DAVID/src/main/java/com/mycompany/fpoe/garces/david/Programa.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.fpoe.garces.david;
+
+/**
+ *
+ * @author David Garces
+ */
+public class Programa {
+    public static void main(String[] args) {
+        Calculadora ui=new Calculadora();
+        ui.setVisible(true);
+    }
+}
